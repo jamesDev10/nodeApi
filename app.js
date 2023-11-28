84e3da2e27e14fd7fecdbbef5b95c829acf7cd95
dev10@finoservices.com <dev10@finoservices.com>
2023-11-28 14:33:58 -0500

    insert files

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ import apiFetch from "node-fetch"
 import {peoples} from "./dateJson/people.js";
 import {templateNoData, templateNoDataFilter} from "./templates/allTemplates.js";
 import {v4 as uuidv4} from 'uuid';
-
+import cors from "cors"
 
 const app = express()
 
-const port = 9861
+const port = 9862
 
 app.use('/public', express.static('public'));
 app.use(express.json())
-
-app.get('/peoplesGet', (req, res) => {
+app.use(cors())
+app.get('/peoples', (req, res) => {
   try {
     if (peoples.length > 0) {
       const constPeople = peoples.map(people => people)
@@ -27,13 +27,12 @@ app.get('/peoplesGet', (req, res) => {
 
 })
 const nuevoId = uuidv4();
-console.log('uii', nuevoId)
 
-app.get('/peoplesGet/:id', (req, res) => {
-  const id = req.params.id
+app.get('/peoples/:name', (req, res) => {
+  const name = req.params.name
   try {
     if (peoples.length > 0) {
-      const constPeople = peoples.filter(people => people.name === id)
+      const constPeople = peoples.filter(people => people.name.toLowerCase() === name.toLowerCase())
       constPeople.length > 0 ? res.json(constPeople) : res.send(templateNoDataFilter)
     } else {
       res.send(templateNoData)
@@ -44,18 +43,30 @@ app.get('/peoplesGet/:id', (req, res) => {
   }
 
 })
-app.post('/peoplesSave', (req, res) => {
 
+app.post('/peoples', (req, res) => {
   const newData = req.body
-
   newData["id"] = nuevoId
   peoples.push(newData)
   res.status(200).json(`${newData.id}`)
 })
 
+app.put('/peoples/:id', (req, res) => {
+  const idUpdate = req.params.id
+  const dataUpdate = req.body
+  const idxPeopled = peoples.findIndex(people => people.id === idUpdate)
+  idxPeopled !== -1 ? peoples[idxPeopled] = dataUpdate : res.json('No se ha podido actualizar')
+  res.json(peoples)
+})
 
+app.delete('/peoples/:id', (req, res) => {
+  const id = req.params.id
+
+  console.log(id)
+  const idxPeopled = peoples.findIndex(people => people.id === id)
+  peoples.splice(idxPeopled, 1)
+  res.json(peoples)
+})
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
 })
-
-
--- dateJson/people.js
+++ dateJson/people.js
@@ -1,4 +1,138 @@
 export const peoples = [
-
-
-  ]
+  {
+    id: 'a1b2c3',
+    name: 'Rafael',
+    lastName: 'Orozco',
+    gender: 'Female',
+    year: 20,
+    occupation: 'Development'
+  },
+  {
+    id: 'd4e5f6',
+    name: 'Laura',
+    lastName: 'Mosquera',
+    gender: 'Female',
+    year: 25,
+    occupation: 'Engineer'
+  },
+  {
+    id: 'g7h8i9',
+    name: 'Alice',
+    lastName: 'Smith',
+    gender: 'Female',
+    year: 25,
+    occupation: 'Marketing'
+  },
+  {
+    id: 'j1k2l3',
+    name: 'Bob',
+    lastName: 'Johnson',
+    gender: 'Male',
+    year: 30,
+    occupation: 'Finance'
+  },
+  {
+    id: 'm4n5o6',
+    name: 'Charlie',
+    lastName: 'Brown',
+    gender: 'Male',
+    year: 22,
+    occupation: 'Design'
+  },
+  {
+    id: 'p7q8r9',
+    name: 'Diana',
+    lastName: 'Miller',
+    gender: 'Female',
+    year: 28,
+    occupation: 'Engineering'
+  },
+  {
+    id: 's1t2u3',
+    name: 'Eva',
+    lastName: 'Jones',
+    gender: 'Female',
+    year: 35,
+    occupation: 'Medical'
+  },
+  {
+    id: 'v4w5x6',
+    name: 'Frank',
+    lastName: 'White',
+    gender: 'Male',
+    year: 40,
+    occupation: 'Education'
+  },
+  {
+    id: 'y7z8a9',
+    name: 'Grace',
+    lastName: 'Davis',
+    gender: 'Female',
+    year: 27,
+    occupation: 'Technology'
+  },
+  {
+    id: 'b1c2d3',
+    name: 'Henry',
+    lastName: 'Clark',
+    gender: 'Male',
+    year: 32,
+    occupation: 'Sales'
+  },
+  {
+    id: 'e4f5g6',
+    name: 'Ivy',
+    lastName: 'Turner',
+    gender: 'Female',
+    year: 29,
+    occupation: 'Art'
+  },
+  {
+    id: 'h7i8j9',
+    name: 'Jack',
+    lastName: 'Walker',
+    gender: 'Male',
+    year: 38,
+    occupation: 'Legal'
+  },
+  {
+    id: 'k1l2m3',
+    name: 'Kelly',
+    lastName: 'Taylor',
+    gender: 'Female',
+    year: 26,
+    occupation: 'Communication'
+  },
+  {
+    id: 'n4o5p6',
+    name: 'Liam',
+    lastName: 'Harris',
+    gender: 'Male',
+    year: 31,
+    occupation: 'Human Resources'
+  },
+  {
+    id: 'q7r8s9',
+    name: 'Mia',
+    lastName: 'Baker',
+    gender: 'Female',
+    year: 24,
+    occupation: 'Research'
+  },
+  {
+    id: 't1u2v3',
+    name: 'Nathan',
+    lastName: 'Woods',
+    gender: 'Male',
+    year: 29,
+    occupation: 'Information Technology'
+  },
+  {
+    id: 'w4x5y6',
+    name: 'Olivia',
+    lastName: 'Cooper',
+    gender: 'Female',
+    year: 33,
+    occupation: 'Public Relations'
+  }
+]
